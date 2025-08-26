fbe3fbcc9369bbd9925508638fe7e2af93273a2f
suleohen <146664090+suleohen@users.noreply.github.com>
2025-08-26 15:54:22 +0300

    template dosyaları eklendi
    SB Admin 2 template eklendi; CSS, JS ve img dosyaları wwwroot içine taşındı, layout için temel dosyalar hazırlandı.

--- obj/Debug/net8.0/GorevNet.AssemblyInfo.cs
+++ obj/Debug/net8.0/GorevNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GorevNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9874145ca4f04d8b2951ec3dccce431a909d1cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae74f124829dfbf780860b574c9dee97a0ec3fe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("GorevNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GorevNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
