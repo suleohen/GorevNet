ae74f124829dfbf780860b574c9dee97a0ec3fe4
suleohen <146664090+suleohen@users.noreply.github.com>
2025-08-26 15:35:17 +0300

    .NET Core MVC boş proje oluşturuldu ve dev branch’e eklendi
    Projeye temel .NET Core MVC yapısı eklendi:
    - Controllers, Models, Views klasörleri oluşturuldu

--- obj/Release/net8.0/GorevNet.AssemblyInfo.cs
+++ obj/Release/net8.0/GorevNet.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("GorevNet")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9874145ca4f04d8b2951ec3dccce431a909d1cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("GorevNet")]
